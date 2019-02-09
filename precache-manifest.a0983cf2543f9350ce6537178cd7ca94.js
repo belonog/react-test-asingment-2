7af9da8fd129904f3e5badc5a2e880f1aa1207fc
Bilonoh Serhii <s.belonog@gmail.com>
2019-02-09 20:41:24 +0200

    Updates

---
+++ precache-manifest.a0983cf2543f9350ce6537178cd7ca94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f56257a4cfcbc7786a5",
+    "url": "/static/js/main.1f56257a.chunk.js"
+  },
+  {
+    "revision": "67299e0ec7e08374442a",
+    "url": "/static/js/1.67299e0e.chunk.js"
+  },
+  {
+    "revision": "1f56257a4cfcbc7786a5",
+    "url": "/static/css/main.d1aa1c8d.chunk.css"
+  },
+  {
+    "revision": "67299e0ec7e08374442a",
+    "url": "/static/css/1.21654f7f.chunk.css"
+  },
+  {
+    "revision": "b1b9548d45e1e78008a1b808ac511696",
+    "url": "/index.html"
+  }
+];
